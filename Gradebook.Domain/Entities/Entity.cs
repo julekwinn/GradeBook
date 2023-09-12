bba4a3c35f16cdc6c584f151211d9e7e5953ba57
Juliusz <julek.winn@gmail.com>
2023-09-12 21:01:11 +0200

    Add class Entity and Student

---
+++ Gradebook.Domain/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Gradebook.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+
+    }
+}

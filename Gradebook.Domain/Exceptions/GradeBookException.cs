c960af98e63c1f6a17664fa127c8b6b654543816
Juliusz <julek.winn@gmail.com>
2023-09-14 14:44:59 +0200

    Add first base exception

--- Gradebook.Domain/Exceptions/GradeBookException.cs
+++ Gradebook.Domain/Exceptions/GradeBookException.cs
@@ -1,7 +1,16 @@
 ﻿
+using System.Net;
+
 namespace Gradebook.Domain.Exceptions
 {
-    internal class GradeBookException
+    public abstract class GradeBookException : Exception
     {
+        public abstract HttpStatusCode StatusCode { get; }
+
+        public GradeBookException(string message) : base(message) 
+        {
+
+        }
+
     }
 }

a0603bff9d5a587d1fb4aa48a824be6c94fac589
Juliusz <julek.winn@gmail.com>
2023-09-14 16:19:15 +0200

    Add services AddInfrastructure

---
+++ GradeBook.Infrastructure/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using GradeBook.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace GradeBook.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GradeBookDbContext>(ctx => ctx.UseSqlServer(configuration.GetConnectionString("GradebookCS")));
+            return services;
+        }
+    }
+}

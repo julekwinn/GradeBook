633255831a0b74a4412238f8d0c73c1a3b3e6238
Juliusz <julek.winn@gmail.com>
2023-09-14 14:57:50 +0200

    Add StudentAlreadyExistException

---
+++ Gradebook.Domain/Exceptions/StudentAlreadyExistException.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Net;
+
+namespace Gradebook.Domain.Exceptions;
+
+internal class StudentAlreadyExistException : GradeBookException
+{
+    public string Email { get; set; } 
+    public StudentAlreadyExistException(string email) : base($"Student with email {email} already exists.")
+    {
+        Email = email;
+    }
+
+    public override HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+
+}

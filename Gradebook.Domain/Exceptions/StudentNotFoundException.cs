576861c66852f3b3c4568f7fae6167a49d03743d
Juliusz <julek.winn@gmail.com>
2023-09-14 14:53:10 +0200

    Add StudentNotFoundException

---
+++ Gradebook.Domain/Exceptions/StudentNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+namespace Gradebook.Domain.Exceptions
+{
+    public class StudentNotFoundException : GradeBookException
+    {
+        public int Id { get; set; }
+        public StudentNotFoundException(int id) : base($"Student with ID{id} not Found") 
+        {
+            Id = id;
+        }
+
+        public override HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+    }
+}

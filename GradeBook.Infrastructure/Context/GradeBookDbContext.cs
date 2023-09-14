ca1d87d49c49f7d742d0a06f78a740523e527896
Juliusz <julek.winn@gmail.com>
2023-09-14 16:04:24 +0200

    Add GradeBookDbContext

---
+++ GradeBook.Infrastructure/Context/GradeBookDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Gradebook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradeBook.Infrastructure.Context
+{
+    internal class GradeBookDbContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+        public GradeBookDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("gradebook");
+        }
+
+
+    }
+}

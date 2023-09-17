44fc574439571c699f62dd1e0f70a9d5f5da62bb
Juliusz <julek.winn@gmail.com>
2023-09-17 15:26:46 +0200

    Finish AddStudentCommandHandler

--- GradeBook.Application/Commands/Students/AddStudent/AddStudentCommandHandler.cs
+++ GradeBook.Application/Commands/Students/AddStudent/AddStudentCommandHandler.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Gradebook.Domain.Abstractions;
+using GradeBook.Application.Dtos;
+using MediatR;
+using Gradebook.Domain.Exceptions;
+using Gradebook.Domain.Entities;
 
-namespace GradeBook.Application.Commands.Students.AddStudent
+namespace GradeBook.Application.Commands.Students.AddStudent;
+
+internal class AddStudentCommandHandler : IRequestHandler<AddStudentCommand, StudentDto>
 {
-    internal class AddStudentCommandHandler
+    private readonly IStudentRepository _studentRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public AddStudentCommandHandler(IStudentRepository studentRepository, IUnitOfWork unitOfWork)
     {
+        _studentRepository = studentRepository;
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<StudentDto> Handle(AddStudentCommand request, CancellationToken cancellationToken)
+    {
+        bool isAlreadyExist = await _studentRepository.IsAlreadyExistAsync(request.Email,cancellationToken);
+        if (isAlreadyExist) 
+        {
+            throw new StudentAlreadyExistException(request.Email);
+        }
+
+        var newStudent = new Student()
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            DateOfBirth = request.DateOfBirth,
+            YearEnrolled = request.YearEnrolled,
+        };
+
+        _studentRepository.Add(newStudent);
+        await _unitOfWork.SaveChangesAsync();
+
+        var studentDto = new StudentDto()
+        {
+            FirstName = newStudent.FirstName,
+            LastName = newStudent.LastName,
+            Email = newStudent.Email,
+            Age = DateTime.Now.Year - newStudent.DateOfBirth.ToDateTime(TimeOnly.Parse("00:00")).Year,
+            YearEnrolled = newStudent.YearEnrolled,
+        };
+
+        return studentDto;
     }
 }
--- Gradebook.Domain/Exceptions/StudentAlreadyExistException.cs
+++ Gradebook.Domain/Exceptions/StudentAlreadyExistException.cs
@@ -3,7 +3,7 @@ using System.Net;
 
 namespace Gradebook.Domain.Exceptions;
 
-internal class StudentAlreadyExistException : GradeBookException
+public class StudentAlreadyExistException : GradeBookException
 {
     public string Email { get; set; } 
     public StudentAlreadyExistException(string email) : base($"Student with email {email} already exists.")

e0433946ec6c3d5d1d06121a302f561a53fef3d4
Juliusz <julek.winn@gmail.com>
2023-09-17 15:28:52 +0200

    Add property to UpdateStudentCommand

---
+++ GradeBook.Application/Commands/Students/UpdateStudent/UpdateStudentCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.Application.Commands.Students.UpdateStudent
+{
+    public class UpdateStudentCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int YearEnrolled { get; set; }
+    }
+}

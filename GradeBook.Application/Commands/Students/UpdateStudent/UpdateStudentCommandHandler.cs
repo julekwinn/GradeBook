9b7512c14f63f06e39ed442e9eb9ae4e08f50371
Juliusz <julek.winn@gmail.com>
2023-09-17 15:42:00 +0200

    Finish UpdateStudent

--- GradeBook.Application/Commands/Students/UpdateStudent/UpdateStudentCommandHandler.cs
+++ GradeBook.Application/Commands/Students/UpdateStudent/UpdateStudentCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Gradebook.Domain.Abstractions;
+using Gradebook.Domain.Exceptions;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,37 @@ using System.Threading.Tasks;
 
 namespace GradeBook.Application.Commands.Students.UpdateStudent
 {
-    internal class UpdateStudentCommandHandler
+ 
+    internal class UpdateStudentCommandHandler : IRequestHandler<UpdateStudentCommand>
     {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateStudentCommandHandler(IStudentRepository studentRepository, IUnitOfWork unitOfWork)
+        {
+            _studentRepository = studentRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(UpdateStudentCommand request, CancellationToken cancellationToken)
+        {
+            var student = await _studentRepository.GetByIdAsync(request.Id, cancellationToken);
+            if (student == null) 
+            {
+                throw new StudentNotFoundException(request.Id);
+            }
+
+            student.FirstName= request.FirstName;
+            student.LastName= request.LastName;
+            student.Email= request.Email;
+            student.DateOfBirth= request.DateOfBirth;
+            student.YearEnrolled= request.YearEnrolled;
+
+            _studentRepository.Update(student);
+            await _unitOfWork.SaveChangesAsync();
+
+
+
+
+        }
     }
 }

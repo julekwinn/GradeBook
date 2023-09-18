8696c8dc3fdec6bc00992d7af1ee5eb53418fcc7
Juliusz <julek.winn@gmail.com>
2023-09-18 11:46:21 +0200

    Add service to controllers

--- GradeBook.Presentation/Extensions.cs
+++ GradeBook.Presentation/Extensions.cs
@@ -10,7 +10,9 @@ public static class Extensions
     public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(swagger => { swagger.EnableAnnotations(); });
+
+        services.AddControllers();
         return services;
     }
 }

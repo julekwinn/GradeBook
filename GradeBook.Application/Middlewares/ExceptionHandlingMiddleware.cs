3487302c4d2c1a9e33d930995dccd34ca9d31044
Juliusz <julek.winn@gmail.com>
2023-09-19 12:21:46 +0200

    Add exception Middleware

---
+++ GradeBook.Application/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Gradebook.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace GradeBook.Application.Middlewares;
+
+public class ExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _logger = logger;
+    }
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e) 
+        {
+            _logger.LogError(e, e.Message);
+            
+            await HandleExceptionAsync(context, e);
+            throw;
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var statusCode = GetStatusCode(exception);
+        var response = new
+        {
+            title = exception.Message,
+            status = statusCode,
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+
+    private static int GetStatusCode(Exception exception) =>
+        exception switch
+        {
+            GradeBookException => (int)(exception as GradeBookException).StatusCode,
+            ValidationException => StatusCodes.Status422UnprocessableEntity,
+            _=> StatusCodes.Status500InternalServerError
+        };
+}
